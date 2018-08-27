a2f1e80c0a9d6dfdbb0e09507b67eb2469fde7ca
aksaba <aksabapathy@gmail.com>
2018-08-27 19:46:48 +0530

    Open url in  browser  code 1

---
+++ Py/Web/openreddit.py
@@ -0,0 +1,11 @@
+import webbrowser
+import sys
+
+chrome_path = 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe %s'
+url = 'https://www.reddit.com/'
+
+webbrowser.get("open -a C:\\Program F~\\Mozilla Firefox\\firefox.exe %s")
+webbrowser.open(url)
+
+#webbrowser.open(url)
+quit()
